3f6291c99059c98fa50df33390046c69db97d384
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2024-05-13 14:03:00 -0300

    Updates nomenclature.
    Updates nomenclature.

---
+++ area/src/main/java/com/br/flavioreboucassantos/devops/controller/ControllerRedirect.java
@@ -0,0 +1,19 @@
+package com.br.flavioreboucassantos.devops.controller;
+
+import java.net.URI;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+@Path("")
+public class ControllerRedirect {
+
+	final Response responseStaticContent = Response.temporaryRedirect(URI.create("/")).build();
+
+	@GET
+	public Response redirect() {
+		return responseStaticContent;
+	}
+
+}
